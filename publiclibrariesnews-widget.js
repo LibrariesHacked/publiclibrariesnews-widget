46d7e2bcce2ab9f431dd33ff03c3d5c2b89c52ef
Dave <info@librarieshacked.org>
2015-02-06 08:02:09 +0000

    Final tweaks before production version

--- publiclibrariesnews-widget.js
+++ publiclibrariesnews-widget.js
@@ -1,8 +1,15 @@
+var map;
+var changesMarkers = [];
+var newsMarkers = [];
+var changesTable = '18RMAdHdkrRwUiUrE7ltKzTdFVjvYQLDGevlczxcm';
+var newsTable = '1juIutd8McD3OGnWHWVrZbyWPeLim9RZXvHRiKmFe';
+var overlays = {};
+
 jQuery(function () {
     // 3 required css files: standard leaflet, fullscreen plugin, and marker cluster style.
     jQuery('head').append('<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css" />');
     jQuery('head').append('<link rel="stylesheet" href="https://api.tiles.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v0.0.4/leaflet.fullscreen.css" />');
-    jQuery('head').append('<link rel="stylesheet" href="http://www.librarieshacked.org/themes/bootstrap/css/MarkerCluster.Default.css" />');
+    jQuery('head').append('<link rel="stylesheet" href="https://cdn.rawgit.com/Leaflet/Leaflet.markercluster/master/dist/MarkerCluster.Default.css" />');
     jQuery('head').append('<style>.leaflet-popup-content-wrapper { border-radius: 0px !important; } .leaflet-popup-content { margin-top: 5px !important; margin-bottom: 5px !important; margin-left: 5px !important; margin-right: 5px !important; } </style>');
 
     // width is fixed to be 100% of the available container.
@@ -10,6 +17,8 @@ jQuery(function () {
 
     // get variables from the map div element.
     var numberItems = jQuery('#plnMap').data('items');
+
+    if (!numberItems) numberItems = '200';
     var height = jQuery('#plnMap').data('height');
 
     // default the height to 250px, if set then use user value.
@@ -20,34 +29,32 @@ jQuery(function () {
     // currently gets them synchronously - may do both asynchronously later.
     jQuery.getScript("http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js", function () {
         jQuery.getScript("https://api.tiles.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v0.0.4/Leaflet.fullscreen.min.js", function () {
-            GetLibraryNews();
+            GetLayer(changesTable, numberItems);
+            GetLayer(newsTable, numberItems);
         });
     });
 });
 
-function GetLibraryNews() {
-    var latlng = L.latLng(53.5, -2.6);
-    var map = L.map('plnMap', { zoomControl: false, center: latlng, zoom: 6, fullscreenControl: true });
+function GetLayer(table, rows) {
 
-    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}' + (L.Browser.retina ? '@2x' : '') + '.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OSM</a> contributors, <a href="http://cartodb.com/attributions">CartoDB</a>'
-    }).addTo(map);
-
-    var fusionQuery = "SELECT Date, Location, Story, Latitude, Longitude, URL FROM 1juIutd8McD3OGnWHWVrZbyWPeLim9RZXvHRiKmFe LIMIT 200"
+    // query to select rows from the fusion table - limited by either 200 (default), or the limit parameter
+    var fusionQuery = "SELECT Date, Location, Story, Latitude, Longitude, URL FROM " + table + " ORDER BY Date DESC LIMIT " + rows;
     var fusionUrl = 'https://www.googleapis.com/fusiontables/v2/query?sql=' + fusionQuery + '&key=AIzaSyCtJBC5LYkc3E1wcS8hgyVWIWxDeiwolAc';
 
+    // will end up grouping data into an object indexed by location.
     var locations = {};
 
     jQuery.getJSON(fusionUrl, function (data) {
+
         if (data && data.rows && data.rows.length > 0) {
 
             jQuery.each(data.rows, function () {
                 if (locations[this[1].trim()] && locations[this[1].trim()].text) {
-                    locations[this[1].trim()].text = locations[this[1].trim()].text + '<h4>' + this[1] + '</h4><small><a href="' + this[5] + '" target="_blank">' + this[0].replace(' +0000','') + '</a></small><br/>' + this[2] + '<br/><br/><hr>';
+                    locations[this[1].trim()].text = locations[this[1].trim()].text + '<h4>' + this[1] + '</h4><small><a href="' + this[5] + '" target="_blank">' + this[0].replace(' +0000', '') + '</a></small><br/>' + this[2] + '<br/><br/><hr>';
                     locations[this[1].trim()].number = locations[this[1].trim()].number + 1;
                 }
                 else {
-                    locations[this[1].trim()] = { text: '<h4>' + this[1] + '</h4><small><a href="' + this[5] + '" target="_blank">' + this[0].replace(' +0000','') + '</a></small><br/>' + this[2] + '<br/><br/><hr>', number: 1, latitude: this[3], longitude: this[4] };
+                    locations[this[1].trim()] = { text: '<h4>' + this[1] + '</h4><small><a href="' + this[5] + '" target="_blank">' + this[0].replace(' +0000', '') + '</a></small><br/>' + this[2] + '<br/><br/><hr>', number: 1, latitude: this[3], longitude: this[4] };
                 }
             });
 
@@ -67,9 +74,35 @@ function GetLibraryNews() {
                         className: 'plnPopup'
                     }).setContent(value.text);
 
-                    L.marker([value.latitude, value.longitude], { icon: newsIcon }).addTo(map).bindPopup(popup);
+                    if (table == newsTable) newsMarkers.push(L.marker([value.latitude, value.longitude], { icon: newsIcon }).bindPopup(popup));
+                    if (table == changesTable) changesMarkers.push(L.marker([value.latitude, value.longitude], { icon: newsIcon }).bindPopup(popup));
                 }
             });
+            
+            if (table == newsTable) overlays['News'] = L.layerGroup(newsMarkers);
+            if (table == changesTable) overlays['Changes'] = L.layerGroup(changesMarkers);
+            
+            // add the layer control if both are loaded
+            if (overlays['Changes'] && overlays['News']) {
+                var latlng = L.latLng(53.5, -2.6);
+                map = L.map('plnMap', { zoomControl: false, center: latlng, zoom: 6, fullscreenControl: true, layers: [overlays['News']] });
+
+                L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}' + (L.Browser.retina ? '@2x' : '') + '.png', {
+                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OSM</a> contributors, <a href="http://cartodb.com/attributions">CartoDB</a>'
+                }).addTo(map);
+
+                L.control.layers(null, overlays).addTo(map);
+
+                // change the overlays to act like radio buttons
+                map.on('overlayadd', function (e) {
+                    if (e.name == 'News') jQuery('#plnMap .leaflet-control-layers-overlays span:contains("Changes")').click();
+                    if (e.name == 'Changes') jQuery('#plnMap .leaflet-control-layers-overlays span:contains("News")').click();
+                });
+                map.on('overlayremove', function (e) {
+                    if (e.name == 'News') jQuery('#plnMap .leaflet-control-layers-overlays span:contains("Changes")').click();
+                    if (e.name == 'Changes') jQuery('#plnMap .leaflet-control-layers-overlays span:contains("News")').click();
+                });
+            }
         }
     });
 }
